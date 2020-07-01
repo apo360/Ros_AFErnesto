ad0a7eff6cc5228a9c3063bbdb4976d588a3b7bb
apo360 <34046439+apo360@users.noreply.github.com>
2020-07-01 14:59:52 +0100

    Update ESP32

--- ESP32/ESP32_VENT_FINAL.ino
+++ ESP32/ESP32_VENT_FINAL.ino
@@ -9,6 +9,7 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 /*-----------------------------------------------------------------*/
+#define BATTERY_PIN 35
 #define INIT_C 0x2B           //código de início de mensagem
 #define TIMEOUT 2000          //prevenção de bloqueio da rotina
 #define MAX_MSG_LENGTH 128
@@ -34,13 +35,18 @@ uint8_t calculateCheckSumXOR(const uint8_t* data, uint16_t n, uint8_t chkSum = 0
 uint8_t replyMessage[MAX_MSG_LENGTH];
 uint8_t message[MAX_MSG_LENGTH];
 uint8_t dataSize, dataCount;
-/*---------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+const String GETDATA = "getdata"; //frases identificativas de
+const String SETDATA = "setdata"; //pedido de transmissão de msg
+/*----------------------------------------------------------------*/
+long lastSendTime = 0;
+/*----------------------------------------------------------------*/
 
 TinyGPSPlus gps;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
-bool status_lora = true, status_imu = true, status_gps = true;
+bool status_lora = true, status_imu = true;
 
 void setup() {
   Serial.begin(9600, SERIAL_8N1, 12, 15);  // Iniciar GPS
@@ -52,10 +58,17 @@ void setup() {
   bno.setExtCrystalUse(true);
   t0 = millis(); //inicialização de todos os componentes
   replyMessage[0] = INIT_C;
+
+// Iniciar o pin de medição da batéria
+  adcAttachPin(BATTERY_PIN);
+  adcStart(BATTERY_PIN);
+  analogReadResolution(10);
+  analogSetPinAttenuation(BATTERY_PIN, ADC_11db);
+// ### Fim Pin Battery ### //
 }
 
 void loop() {
-
+  
   int packetSize = LoRa.parsePacket();
   
   if (packetSize) 
@@ -66,25 +79,31 @@ void loop() {
       
       message_struct(Formato_lora, REPLY_LoRa);
     }
+    
   }
-
-  if(Serial2.available()){
-
-    String pedido = Serial2.readString();
-
-    if(pedido.substring(0, 3) == "RPY")
-    {
-      readData();
-    } 
-    else if (pedido.substring(0, 4) == "LoRa")
-    {
-      LoRa.beginPacket();
-      LoRa.println(pedido);
-      LoRa.endPacket();
-    }
-    else {Serial2.println("Erro na entrada de Pedidos");}
-  }
+  else if(!packetSize)
+  {
+    if(Serial2.available()){
+      
+      String pedido = Serial2.readString();
   
+      if(strcmp(pedido.substring(0, 3).c_str(),"RPY") == 0)
+      {
+        readData();
+      } 
+      else if (pedido.substring(0, 4) == "LoRa")
+      {
+        LoRa.beginPacket();
+        LoRa.println(pedido);
+        LoRa.endPacket();
+      }
+      else if (strcmp(pedido.c_str(),"restart") == 0)
+      {
+        delay(5000);
+        ESP.restart();
+      }
+    }
+  }
 }
 
 static void smartDelay(unsigned long ms){
@@ -125,90 +144,77 @@ void message_struct(String FinalValue, uint8_t REPLAY){
 
 void Hardeware_Status(void){
   
-  String estado, lora_estado;
+  String estado, lora_estado, imu_estado;
+  sensor_t sensor;
+  bno.getSensor(&sensor);
 
-  if(status_lora){lora_estado =("LoRa " + String(BAND)+ ' ' + String(bno.getTemp()) + " Conectado");}
-  else{lora_estado = ("LoRa " + String(BAND)+ ' ' + String(bno.getTemp()) + " Desconectado");}
+  String battery = "Battery Voltage :" + String(analogRead(BATTERY_PIN)*2.0*(3.3/1024.0));
+  
+  imu_estado = String(sensor.name)+ ' ' + String(bno.getTemp()) + " Conectado";
 
-  estado = "$1:" + ','+ lora_estado + '\n';
+  if(status_lora){lora_estado =("LoRa-" + String(BAND)+ ' ' + " Conectado");}
+  else{lora_estado = ("LoRa-" + String(BAND)+ ' ' + " Desconectado");}
+
+  estado = "$Hardware:" + imu_estado + ','+ lora_estado +','+battery+','+ '\n';
 
   message_struct(estado, REPLY_GPS_SATELITES);
 }
 
 void Imu_Status(void){
-
-  double x, y, z; //dumb values, easy to spot problem
-  String imu_estado;
   
-  sensors_event_t orient_;
-  sensor_t sensor;
+  sensors_event_t orient_, gravity_, gyroscope_, linearAccel_, magnetometer_;
   
-  bno.getSensor(&sensor);
   bno.getEvent(&orient_, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&gravity_, Adafruit_BNO055::VECTOR_GRAVITY);
+  bno.getEvent(&gyroscope_, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccel_, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  bno.getEvent(&magnetometer_, Adafruit_BNO055::VECTOR_MAGNETOMETER);
 
-  if(status_imu){ imu_estado = (String(sensor.name)+ ' ' + String(bno.getTemp()) + " Conectado");}
-  else{imu_estado = (String(sensor.name)+ ' ' + String(bno.getTemp()) + " Desconectado");}
+  String imuOrient_ = String(orient_.orientation.x) + ' ' + String(orient_.orientation.y) + ' ' + String(orient_.orientation.z);
+  String imuGravity_ = String(gravity_.orientation.x) + ' ' + String(gravity_.orientation.y) + ' ' + String(gravity_.orientation.z);
+  String imuGyroscope_ = String(gyroscope_.orientation.x) + ' ' + String(gyroscope_.orientation.y) + ' ' + String(gyroscope_.orientation.z);
+  String imuLinearccel_ = String(linearAccel_.orientation.x) + ' ' + String(linearAccel_.orientation.y) + ' ' + String(linearAccel_.orientation.z);
+  String imuMagnetometer_ = String(magnetometer_.orientation.x) + ' ' + String(magnetometer_.orientation.y) + ' ' + String(magnetometer_.orientation.z);
 
-    x = orient_.orientation.x;
-    y = orient_.orientation.y;
-    z = orient_.orientation.z;
-
-    String imu_ = "$HCHDT:" + String(x) + ' ' + String(y) + ' ' + String(z) + ' ' + imu_estado + '\0';
+  String imu_ = "$HCHDT:" + imuOrient_ + ',' + imuGravity_ + ',' + imuGyroscope_ + ',' + imuLinearccel_ + ',' + imuMagnetometer_ + ',' + '\0';
 
   message_struct(imu_, REPLY_IMU);
 }
 
 void Gps_Status(void){
-
-  String gps_estado;
   
   //leitura da latitude e transformação em formato NMEA
   float latitude = gps.location.lat();
-  
   if (latitude < 0) latitude = latitude * -1;
-  
-  int latitudeD = floor(latitude);
-  
-  float latitudeDM = ((latitude - latitudeD) * 60);
 
   //leitura da longitude e transformação em formato NMEA
   float longitude = gps.location.lng();
-  
   if (longitude < 0) longitude = longitude * -1;
-  
-  int longitudeD = floor(longitude);
-  int longitudeD0 = longitudeD * 10;
-  float longitudeDM = ((longitude - longitudeD) * 60);
 
   String UTC = String((gps.time.value())/100) + "Z"; //Formato Grupo data e Hora
+  
   String equador = (gps.location.rawLng().negative ? "N" : "S");
+  
   String meridiano = (gps.location.rawLng().negative ? "W" : "E");
+  
   double sog = (gps.speed.knots()); //leitura da velocidade em nós
+  
   double cog = (gps.course.deg()); // Leitura do rumo
-  String ddate = String(gps.date.value()); //leitura da hora
   
-  static const double GS_LAT = 38.6631, GS_LON = -9.146906;
+  String ddate = String(gps.date.month()) + F("/") + String(gps.date.day()) + F("/") + String(gps.date.year()); //leitura da hora
   
-  double distanceToGS = TinyGPSPlus::distanceBetween(
-                          gps.location.lat(),
-                          gps.location.lng(),
-                          GS_LAT,
-                          GS_LON);
+  String satelites = String(gps.satellites.value()); // leitura de satélites
 
-  if (millis() > 5000 && gps.charsProcessed() > 10){gps_estado = String(gps.satellites.value())+ ' ' + "Conectado";}
-  else{gps_estado = String(gps.satellites.value()) + ',' + "Conectado";}
-
-  String gps_ = "$GPGGA:" + UTC + ',' + String(latitudeD) + String(latitudeDM,5) + ' ' + String(equador) + ',' +
-  String(longitudeD0) + String(longitudeDM, 5) + ' ' + String(meridiano) + ',' + String(sog) + ',' + 
-  String(cog) + ',' + String(ddate) + ',' + gps_estado + '\0';
+  String gps_ = "$GPGGA:" + UTC + ',' + String(latitude,5) + ' ' + String(equador) + ',' + String(longitude, 5) + ' ' + String(meridiano) + ',' + String(sog) + ',' + String(cog) + ',' + String(ddate) + ',' + satelites +',' +'\0';
 
   message_struct(gps_, REPLY_GPS);
+
 }
 
 void readData(){
 
-  //Hardeware_Status();
+  Hardeware_Status();
   Imu_Status();
   Gps_Status();
-  smartDelay(500);
+  smartDelay(50);
 }
